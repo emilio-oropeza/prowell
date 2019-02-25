919baebc201e6c82525b5c90dae42b106a008f19
vocho <vocho17@gmail.com>
2019-02-24 23:10:17 -0600

    Agregados 2 secciones

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@
 //     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 //   });
 // });
+
+const returnRandom = () => Math.floor(Math.random() * 5) + 1;
+const characters = Array.from(document.querySelectorAll('.character_img'));
+characters.forEach(character => character.style.animation = `mover ${returnRandom()}s infinite  alternate`)
